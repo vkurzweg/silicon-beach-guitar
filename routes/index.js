811bf387b48da08dbfb23484774394cea1f0cc9c
Victoria Dunham <vkurzweg@gmail.com>
2017-06-19 22:25:10 -0700

    add nav

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Silicon Beach Guitar Lessons' });
+});
+
+router.get('/rates', function(req, res, next) {
+  res.render('rates', { title: 'Silicon Beach Guitar Lessons' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Silicon Beach Guitar Lessons' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Silicon Beach Guitar Lessons' });
 });
 
 module.exports = router;
