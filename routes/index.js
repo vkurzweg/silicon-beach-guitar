0e631a146eb28ad28793e72149f0b12977ab17aa
Victoria Dunham <vkurzweg@gmail.com>
2017-06-29 18:35:24 -0700

    set up contact form email notification

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const sendmail = require('sendmail')({
+  logger: {
+    debug: console.log,
+    info: console.info,
+    warn: console.warn,
+    error: console.error
+  }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +24,22 @@ router.get('/about', function(req, res, next) {
 
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Silicon Beach Guitar Lessons' });
+});
+
+router.post('/contactus', function(req, res, next) {
+  console.log(req.body)
+  sendmail({
+      from: req.body.contactEmail,
+      to: 'victoriakdunham@gmail.com',
+      subject: 'Silicon Beach Guitar Lessons',
+      html: '<p>name: ' + req.body.contactName +'</p>' + '<p>email: ' + req.body.contactEmail + '</p>' + '<p>number: ' + req.body.contactPhone +'</p>' + '<p>message: ' + req.body.contactMessage + '</p>'
+    }, function(err, reply) {
+      console.log(err && err.stack);
+      if (reply.indexOf('221') >= 0){
+        res.status(200).send('Message sent');
+      }
+      console.dir(reply);
+  });
 });
 
 module.exports = router;
